d37c620ac92bff12ec1311b3e407b5d3642b593d
elsteiner <elmar.steiner@uni-graz.at>
2023-08-03 13:48:36 +0200

    additional tests

--- TestFile.jl
+++ TestFile.jl
@@ -1,1 +1,21 @@
-println("Hello World")
+
+#= 
+Test File 
+Oegor Workshop
+=#
+zipcode = 4020
+
+
+function affinect(x::Int64, b::Int64=9)
+    println("Welcome to ", ifelse(zipcode==4020, "Linz", "Austria"))
+    for a in "string", b in "hello"
+        print(a, b, "-")
+    end
+    y = x^2 + b
+    return y
+end
+
+println(affinect(2,9))
+
+println("Hello Krems ")
+
